c165b7547cd09ea2b3e24a6fb4fe4a0d6d61a4eb
PatioK <krzysztofswnsk@gmail.com>
2025-11-20 13:06:52 +0100

    zadanie 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
+  const total = good + neutral + bad
+  const average = total === 0
+    ? 0
+    : (good * 1 + bad * -1) / total
+  const positivePercentage = total === 0
+    ? '0%'
+    : `${(good / total * 100).toFixed(1)}%`
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h2>statistics</h2>
+      <div>good: {good}</div>
+      <div>neutral: {neutral}</div>
+      <div>bad: {bad}</div>
+      <div>all: {total}</div>
+      <div>average: {average}</div>
+      <div>positive: {positivePercentage}</div>
     </div>
   )
 }
