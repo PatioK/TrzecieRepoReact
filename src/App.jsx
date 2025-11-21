7c4d09924e4f84bd6ee50bb8a7692aa0cc1c35be
PatioK <krzysztofswnsk@gmail.com>
2025-11-21 13:19:32 +0100

    zadanie 1.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,29 @@
-import { useState } from 'react'
-
-const StatisticLine = ({text, value}) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad
-  if (all === 0) {
-    return <div>No feedback given</div>
-  }
-
-  const average = (good - bad) / all
-  const positive = (good / all) * 100 + " %"
-
-  return (
-    <table>
-      <tbody>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-      </tbody>
-    </table>
-  )
-}
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
-)
+import { useState } from 'react';
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ];
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
-  
-  const handleReset = () => {
-    setGood(0)
-    setNeutral(0)
-    setBad(0)
-  }
+  const [selected, setSelected] = useState(0);
+
+  const handleNext = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button onClick={handleGood} text="good" />
-      <Button onClick={handleNeutral} text="neutral" />
-      <Button onClick={handleBad} text="bad" />
-
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <button onClick={handleNext}>Next anecdote</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
