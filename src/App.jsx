d55eae2be92f49fec49f54031a49da8e8291b09e
PatioK <krzysztofswnsk@gmail.com>
2025-11-27 13:25:14 +0100

    zadanie 1.13

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often',
+    'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ];
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const [selected, setSelected] = useState(0);
+  // Tworzymy tablicę głosów, początkowo same zera
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  // Index aktualnie wyświetlanej anegdoty
+  const [selected, setSelected] = useState(0)
 
   const handleNext = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomIndex);
-  };
+    const next = Math.floor(Math.random() * anecdotes.length)
+    setSelected(next)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const voteNum = () => {
+    
+  }
 
   return (
     <div>
       <h1>Anecdote of the day</h1>
       <div>{anecdotes[selected]}</div>
-      <button onClick={handleNext}>Next anecdote</button>
+      <div>has {votes[selected]} votes</div>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>next anecdote</button>
     </div>
-  );
-};
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
 
 export default App;
