1dfecbb6c6f8fb52876cb6a4dccbcbecebf53956
PatioK <krzysztofswnsk@gmail.com>
2026-01-30 12:37:46 +0100

    zadanie 2.10

--- src/App.jsx
+++ src/App.jsx
@@ -33,27 +33,31 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          <form>
-            <div>name: <input /></div>
-            <div>number: <input /></div>
-          </form>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
 
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
-        )}
-      </ul>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
