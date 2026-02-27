4bddf306f62afd5a068428ccbb51a4e62c53a670
PatioK <krzysztofswnsk@gmail.com>
2026-02-27 13:05:22 +0100

    zadanie 2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,33 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [persons, setPersons] = useState([]);
+
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
       })
-  }, [])
-
-   const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
-    }
-
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
+      .catch((error) => {
+        console.error("Error fetching persons:", error);
+      });
+  }, []);
 
   return (
     <div>
-      <h2>Phonebook</h2>
-
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
-        </div>
-        <button type="submit">add</button>
-      </form>
-
-      <h2>Numbers</h2>
-      {persons.map(p => (
-        <p key={p.id}>{p.name} {p.number}</p>
-      ))}
+      <h1>Phonebook</h1>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
