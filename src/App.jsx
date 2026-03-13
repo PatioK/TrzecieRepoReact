94797b216feef16cf1e465673435fb56251d20a6
PatioK <krzysztofswnsk@gmail.com>
2026-03-13 12:58:34 +0100

    2.12

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,6 @@ const App = () => {
       .then(response => {
         setPersons(response.data)
       })
-      .catch(error => {
-        console.error('Error fetching persons:', error)
-      })
   }, [])
 
   const addPerson = (event) => {
@@ -22,28 +19,17 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
-
-  const addNote = event => {
-  event.preventDefault()
-  const noteObject = {
-    content: newNote,
-    important: Math.random() < 0.5,
-  }
-
-  axios
-    .post('http://localhost:3001/persons', noteObject)
-    .then(response => {
-      console.log(response)
-    })
-}
 
   return (
     <div>
@@ -51,28 +37,23 @@ const App = () => {
 
       <form onSubmit={addPerson}>
         <div>
-          name:{' '}
-          <input
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
+          name: 
+          <input value={newName} onChange={(e)=>setNewName(e.target.value)} />
         </div>
         <div>
-          number:{' '}
-          <input
-            value={newNumber}
-            onChange={(e) => setNewNumber(e.target.value)}
-          />
+          number:
+          <input value={newNumber} onChange={(e)=>setNewNumber(e.target.value)} />
         </div>
         <button type="submit">add</button>
       </form>
 
-      <h2>Numbers</h2>
-      {persons.map(p => (
-        <p key={p.id}>
-          {p.name} {p.number}
-        </p>
-      ))}
+      <ul>
+        {persons.map(p =>
+          <li key={p.id}>
+            {p.name} {p.number}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
