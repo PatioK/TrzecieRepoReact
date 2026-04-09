b8cc82531012a6f35c38650bfa0508b2dd86e428
PatioK <krzysztofswnsk@gmail.com>
2026-04-09 13:19:31 +0200

    2.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  // 🔹 pobieranie danych z backendu
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -22,8 +23,8 @@ const App = () => {
       number: newNumber
     }
 
-    axios
-      .post('http://localhost:3001/persons', personObject)
+    personService
+      .create(personObject)
       .then(response => {
         setPersons(persons.concat(response.data))
         setNewName('')
@@ -37,23 +38,30 @@ const App = () => {
 
       <form onSubmit={addPerson}>
         <div>
-          name: 
-          <input value={newName} onChange={(e)=>setNewName(e.target.value)} />
+          name:
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
         </div>
+
         <div>
           number:
-          <input value={newNumber} onChange={(e)=>setNewNumber(e.target.value)} />
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
+
         <button type="submit">add</button>
       </form>
 
-      <ul>
-        {persons.map(p =>
-          <li key={p.id}>
-            {p.name} {p.number}
-          </li>
-        )}
-      </ul>
+      <h2>Numbers</h2>
+      {persons.map(p => (
+        <p key={p.id}>
+          {p.name} {p.number}
+        </p>
+      ))}
     </div>
   )
 }
